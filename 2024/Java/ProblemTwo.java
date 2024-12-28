18ff3fe3affbb3ae3a76c6136e17cf9b92fb46eb
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-28 14:54:35 -0800

    problem 2

--- 2024/Java/ProblemTwo.java
+++ 2024/Java/ProblemTwo.java
@@ -1,5 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
 public class ProblemTwo {
+    static String readInFile(String filePath) {
+        String fileInput = "";
+        String tempString = "";
+
+        try {
+            Scanner fileReader = new Scanner(new File(filePath));
+            while (fileReader.hasNextLine()) {
+                tempString = fileReader.nextLine();
+                fileInput = fileInput + tempString + "\n";
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return fileInput;
+    }
+    static void parseInput(String fileInput) {
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileInput = readInFile("Inputs/test.txt");
     }
 }
