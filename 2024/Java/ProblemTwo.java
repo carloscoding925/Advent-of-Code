5e51c186a7ad90ef6db00a02672eadbc833106df
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-29 01:36:23 -0800

    working on fix

--- 2024/Java/ProblemTwo.java
+++ 2024/Java/ProblemTwo.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.util.Scanner;
 
 public class ProblemTwo {
+
     static String readInFile(String filePath) {
         String fileInput = "";
         String tempString = "";
@@ -21,11 +22,100 @@ public class ProblemTwo {
 
         return fileInput;
     }
-    static void parseInput(String fileInput) {
-        
+
+    static String[] parseInput(String fileInput) {
+        String[] fileLines = fileInput.split("\n");
+        String[] reportNumbers = new String[fileLines.length];
+
+        for (int i = 0; i < fileLines.length; i++) {
+            String[] line = fileLines[i].split(" ");
+
+            for (int j = 0; j < line.length; j++) {
+                if (reportNumbers[i] == null) {
+                    reportNumbers[i] = line[j];
+                }
+                else {
+                    reportNumbers[i] = reportNumbers[i].concat(line[j]);
+                }
+            }
+        }
+
+        return reportNumbers;
+    }
+
+    static void solvePartOne(String[] reportNumbers) {
+        int safeReports = 0;
+
+        for (int i = 0; i < reportNumbers.length; i++) {
+            boolean isSafe = false;
+            int changeAmount = 0;
+            String direction = "increasing";
+
+            int checkOne = Character.getNumericValue(reportNumbers[i].charAt(0));
+            int checkTwo = Character.getNumericValue(reportNumbers[i].charAt(1));
+
+            if (checkOne - checkTwo > 0) {
+                direction = "decreasing";
+            }
+            else if (checkOne - checkTwo < 0) {
+                direction = "increasing";
+            }
+            else if (checkOne - checkTwo == 0) {
+                isSafe = false;
+                continue;
+            }
+
+            for (int j = 0; j < reportNumbers[i].length() - 1; j++) {
+                int numOne = Character.getNumericValue(reportNumbers[i].charAt(j));
+                int numTwo = Character.getNumericValue(reportNumbers[i].charAt(j + 1));
+
+                if (direction == "increasing") {
+                    changeAmount = numTwo - numOne;
+
+                    if (changeAmount <= 0) {
+                        isSafe = false;
+                        break;
+                    }
+                    else if (changeAmount > 3) {
+                        isSafe = false;
+                        break;
+                    }
+                    else if (changeAmount <= 3) {
+                        isSafe = true;
+                    }
+                }
+                else {
+                    changeAmount = numOne - numTwo;
+
+                    if (changeAmount <= 0) {
+                        isSafe = false;
+                        break;
+                    }
+                    else if (changeAmount > 3) {
+                        isSafe = false;
+                        break;
+                    }
+                    else if (changeAmount <= 3) {
+                        isSafe = true;
+                    }
+                }
+            }
+            
+            if (isSafe) {
+                safeReports++;
+                System.out.println("Is safe: " + reportNumbers[i]);
+            }
+        }
+
+        System.out.println("The number of safe reports is: " + safeReports);
     }
 
     public static void main(String[] args) {
         String fileInput = readInFile("Inputs/test.txt");
+        String[] reportNumbers = parseInput(fileInput);
+        
+        // Error in parseInput, does not work for double digit numbers, need to fix
+        
+        solvePartOne(reportNumbers);
     }
 }
