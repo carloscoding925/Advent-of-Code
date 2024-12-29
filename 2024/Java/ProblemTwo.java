963bc4bfecebc00e07d0b4147382e3bbcbdf430c
Carlos Hernandez <carlosandresh925@gmail.com>
2024-12-29 13:57:55 -0800

    working on part 2

--- 2024/Java/ProblemTwo.java
+++ 2024/Java/ProblemTwo.java
@@ -23,21 +23,6 @@ public class ProblemTwo {
         return fileInput;
     }
 
-    static int[][] parseInput(String fileInput) {
-        String[] fileLines = fileInput.split("\n");
-        int[][] reportNumbers = new int[fileLines.length][10];
-
-        for (int i = 0; i < fileLines.length; i++) {
-            String[] line = fileLines[i].split(" ");
-
-            for (int j = 0; j < line.length; j++) {
-                reportNumbers[i][j] = Integer.parseInt(line[j]);
-            }
-        }
-
-        return reportNumbers;
-    }
-
     static void solvePartOne(String fileInput) {
         String[] fileLines = fileInput.split("\n");
         int safeReports = 0;
@@ -107,8 +92,33 @@ public class ProblemTwo {
         // Answer should be: 421 Safe reports
     }
 
+    static void solvePartTwo(String fileInput) {
+        String[] fileLines = fileInput.split("\n");
+        int safeReports = 0;
+
+        for (int i = 0; i < fileLines.length; i++) {
+            String reportNumbers[] = fileLines[i].split(" ");
+            boolean isSafe = false;
+            int changeAmount = 0;
+            String direction = "increasing";
+
+            int checkOne = Integer.parseInt(reportNumbers[0]);
+            int checkTwo = Integer.parseInt(reportNumbers[1]);
+
+            if (checkOne - checkTwo > 0) {
+                direction = "decreasing";
+            }
+            else if (checkOne - checkTwo < 0) {
+                direction = "increasing";
+            }
+            else if (checkOne - checkTwo == 0) {
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String fileInput = readInFile("Inputs/ProblemTwo.txt");
+        String fileInput = readInFile("Inputs/test.txt");
         solvePartOne(fileInput);
     }
 }
