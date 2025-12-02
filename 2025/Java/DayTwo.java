2fe0a1fa50aff8d32dddae5d8dedc7ed31fd13ee
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-01 21:56:45 -0800

    First attempt at part 2

--- 2025/Java/DayTwo.java
+++ 2025/Java/DayTwo.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class DayTwo {
     public static void main(String[] args) {
         solvePartOne();
+        solvePartTwo();
 
         return;
     }
@@ -62,7 +63,58 @@ public class DayTwo {
                 }
             }
 
-            System.out.println("Total Sum: " + sum);
+            System.out.println("Part 1 Total Sum: " + sum);
+            // Should be 64215794229
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
+    }
+
+    private static void solvePartTwo() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("../Inputs/DayTwo.txt"));
+
+            List<String> allRanges = new ArrayList<>();
+            for (String line : lines) {
+                String[] ranges = line.split(",");
+
+                for (String range : ranges) {
+                    if (!range.trim().isEmpty()) {
+                        allRanges.add(range.trim());
+                    }
+                }
+            }
+
+            long sum = 0;
+            for (String range : allRanges) {
+                String[] ids = range.split("-");
+                long left = Long.parseLong(ids[0]);
+                long right = Long.parseLong(ids[1]);
+
+                while (left <= right) {
+                    String idString = String.valueOf(left);
+                    int idLength = idString.length();
+                    boolean hasSequence = false;
+
+                    for (int sequenceLength = 1; sequenceLength <= idLength / 2; sequenceLength++) {
+                        if (idLength % sequenceLength == 0) {
+                            String seqString = idString.substring(0, sequenceLength);
+                            int repetitions = idLength / sequenceLength;
+
+                            if (seqString.repeat(repetitions).equals(idString)) {
+                                hasSequence = true;
+                            }
+                        }
+                    }
+
+                    if (hasSequence) {
+                        sum = sum + left;
+                    }
+                }
+            }
+
+            System.out.println("Part 2 Total Sum: " + sum);
+            // Should be
         } catch (Exception ex) {
             System.out.println("Caught Exception: " + ex);
         }
