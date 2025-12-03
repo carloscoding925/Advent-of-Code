3a463359e8d8800730c5313e2ade6679f8e1409f
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-02 21:06:51 -0800

    Init day three :-)

---
+++ 2025/Java/DayThree.java
@@ -0,0 +1,15 @@
+public class DayThree {
+    public static void main(String[] args) {
+        solvePartOne();
+
+        return;
+    }
+
+    private static void solvePartOne() {
+        try {
+
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
+    }
+}
