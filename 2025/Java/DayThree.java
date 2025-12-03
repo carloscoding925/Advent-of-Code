0f0f9b763969defeded2fd2640aaa4d44e64a4bf
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-02 21:20:43 -0800

    nested loop maybe

--- 2025/Java/DayThree.java
+++ 2025/Java/DayThree.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class DayThree {
     public static void main(String[] args) {
         solvePartOne();
@@ -7,7 +10,23 @@ public class DayThree {
 
     private static void solvePartOne() {
         try {
+            Scanner scanner = new Scanner(new File("../Inputs/DayThree.txt"));
+            int maxJoltage = 0;
 
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                int largest = 0;
+
+                for (int i = 0; i < line.length() - 1; i++) {
+                    for (int j = 0; j < line.length(); j++) {
+                        
+                    }
+                }
+            }
+
+            System.out.println("Max Joltage: " + maxJoltage);
+
+            scanner.close();
         } catch (Exception ex) {
             System.out.println("Caught Exception: " + ex);
         }
