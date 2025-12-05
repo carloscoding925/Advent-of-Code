3c76ba6c9f966c27dd5136a9a893d5f4a36f2f3a
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-04 21:35:31 -0800

    Hardest part done

--- 2025/Java/DayFive.java
+++ 2025/Java/DayFive.java
@@ -1,3 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DayFive {
     public static void main() {
         solvePartOne();
@@ -7,7 +12,32 @@ public class DayFive {
     }
 
     private static void solvePartOne() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("../Inputs/DayFive.txt"));
 
+            List<String> ranges = new ArrayList<>();
+            List<Integer> ids = new ArrayList<>();
+
+            boolean hitDelimiter = false;
+
+            for (String line : lines) {
+                if (line.isBlank()) {
+                    hitDelimiter = true;
+                    continue;
+                }
+
+                if (!hitDelimiter) {
+                    ranges.add(line);
+                }
+                else {
+                    ids.add(Integer.parseInt(line));
+                }
+            }
+
+            System.out.println(ranges.toString() + " " + ids.toString());
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
     }
 
     private static void solvePartTwo() {
