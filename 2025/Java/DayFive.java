8c1e95dd2d76ce855b92fe51c14010a0a09750cf
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-04 21:24:19 -0800

    init day 5

---
+++ 2025/Java/DayFive.java
@@ -0,0 +1,16 @@
+public class DayFive {
+    public static void main() {
+        solvePartOne();
+        solvePartTwo();
+
+        return;
+    }
+
+    private static void solvePartOne() {
+
+    }
+
+    private static void solvePartTwo() {
+
+    }
+}
