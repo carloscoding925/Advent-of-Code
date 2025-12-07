020a058904f2e46b812cf042200ddfb4dc4931c3
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-06 19:29:34 -0800

    Parse file

--- 2025/Java/DaySix.java
+++ 2025/Java/DaySix.java
@@ -1,3 +1,9 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DaySix {
     public static void main(String[] args) {
         solvePartOne();
@@ -7,7 +13,26 @@ public class DaySix {
     }
 
     private static void solvePartOne() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("../Inputs/DaySix.txt"));
+            int inputLength = lines.size();
 
+            List<String[]> nums = new ArrayList<>();
+            for (int i = 0; i < inputLength - 1; i++) {
+                String[] lineValues = lines.get(i).trim().split("\\s+");
+                nums.add(lineValues);
+            }
+
+            String operationString = lines.getLast();
+            String[] operations = operationString.trim().split("\\s+");
+
+            for (int i = 0; i < nums.size(); i++) {
+                System.out.println(Arrays.toString(nums.get(i)));
+            }
+            System.out.println(Arrays.toString(operations));
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
     }
 
     private static void solvePartTwo() {
