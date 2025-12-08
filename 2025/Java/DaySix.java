050389cfce39dc4bbedf5ba0953932fa1718a8f4
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-08 14:38:31 -0800

    Process file for part 2

--- 2025/Java/DaySix.java
+++ 2025/Java/DaySix.java
@@ -1,7 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DaySix {
@@ -57,6 +56,18 @@ public class DaySix {
     }
 
     private static void solvePartTwo() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("../Inputs/DaySix.txt"));
+            int inputLength = lines.size();
 
+            List<String> nums = new ArrayList<>();
+            for (int i = 0; i < inputLength - 1; i++) {
+                nums.add(lines.get(i));
+            }
+
+            String operationString = lines.getLast();
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
     }
 }
