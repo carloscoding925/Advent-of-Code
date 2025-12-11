8df623217052661d074550b4857f9c04d54e22c9
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-11 11:35:04 -0800

    Form graph from machines

--- 2025/Java/DayEleven.java
+++ 2025/Java/DayEleven.java
@@ -1,3 +1,10 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DayEleven {
     public static void main(String[] args) {
         solvePartOne();
@@ -7,7 +14,21 @@ public class DayEleven {
     }
 
     private static void solvePartOne() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("../Inputs/DayEleven.txt"));
 
+            Map<String, List<String>> graph = new HashMap<>();
+
+            for (String line : lines) {
+                String[] parts = line.split(": ");
+                String machine = parts[0];
+                String[] outputs = parts[1].split(" ");
+
+                graph.put(machine, Arrays.asList(outputs));
+            }
+        } catch (Exception ex) {
+            System.out.println("Caught Exception: " + ex);
+        }
     }
 
     private static void solvePartTwo() {
